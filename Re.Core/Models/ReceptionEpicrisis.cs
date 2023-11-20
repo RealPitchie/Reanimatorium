c692c8b115d240d70e7482ff773186ddea2263a8
Kirill <futb.kirill8888@yandex.ru>
2023-11-20 14:29:00 +0300

    Add OnInitialized

--- Re.Core/Models/ReceptionEpicrisis.cs
+++ Re.Core/Models/ReceptionEpicrisis.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Formats.Asn1;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 #nullable disable
 namespace Re.Core.Models
