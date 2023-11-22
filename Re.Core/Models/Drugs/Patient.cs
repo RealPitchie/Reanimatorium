a2ca9072b53dda1d63ccff454014df705ba5d60c
Никита Камалетдинов <89829621077@bk.ru>
2023-11-22 14:32:24 +0300

    Add fields & fix design

--- Re.Core/Models/Drugs/Patient.cs
+++ Re.Core/Models/Drugs/Patient.cs
@@ -1,12 +1,26 @@
 namespace Re.Core.Models;
 
 public class Patient{
-     public string FIO { get; set; }
 
-     public string Adress { get; set; }
+    public int Id { get; set; }//ID
 
-     public int ArtPresUp { get; set; }
-    public int ArtPresDown { get; set; }
+    public string FIO { get; set; }//ФИО
 
-    public int Pulse { get; set; }
+    public string Adress { get; set; }//Место?
+
+    public string Peculiarity { get; set; }//Особенности пульса
+
+    public string VAS { get; set; }//ВАШ
+
+    public string Notes { get; set; }//Дополнительно
+
+    public int ArtPresUp { get; set; }//Систолическое давление
+
+    public int ArtPresDown { get; set; }//Диастолическое давление
+
+    public int Pulse { get; set; }//Пульс
+
+    public int SpO2 { get; set; }//Насыщение кислородом
+
+
 }
