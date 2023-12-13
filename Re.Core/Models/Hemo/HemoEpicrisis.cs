abffa4eaedb145ab864db78fe9f7ac35050670d8
kixserxx <averyanovayanaa@gmail.com>
2023-12-13 14:19:44 +0300

    @bind-Value + doctor + design changed for HemoEpicrisis

--- Re.Core/Models/Hemo/HemoEpicrisis.cs
+++ Re.Core/Models/Hemo/HemoEpicrisis.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Re.Core.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#nullable disable
 
 namespace Re.Core.Models.Hemo
 {
@@ -11,14 +13,19 @@ namespace Re.Core.Models.Hemo
         public string Id { get; set; }
         public DateOnly Date { get; set; }
         public TimeOnly Time { get; set; }
-        public string Name { get; set; }
         public string FIO { get; set; } //больной(-ая)
         public string Age { get; set; } //возраст
         public string Number { get; set; } //история болезни
-        public string[] GroupRh { get; set; } //группа крови и Rh-принадлежность 
+        public string GroupRh { get; set; } //группа крови и Rh-принадлежность 
         public string Diagnosis { get; set; } //диагноз
-        public string[] Indications { get; set; } //показания к переливанию
+        public string Indications { get; set; } //показания к переливанию
         public string Hemotransfusion { get; set; } //ранее гемотрансфузии
-        public string[] Doctor { get; set; } //доктор
+        public string danerit { get; set; } //кол-во эритроцит. взвеси
+        public string EritGroup { get; set; } //группа эритроцит. взвеси
+        public string danszp { get; set; } //кол-во свежезам. плазмы
+        public string SZPGroup { get; set; } //группа свежезам. плазмы
+        public string dantromb { get; set; } //кол-во тромбоцит. конц.
+        public string TrombGroup { get; set; } //группа тромбоцит. конц.
+        public Doctor Doctor { get; set; } //доктор
     }
 }
